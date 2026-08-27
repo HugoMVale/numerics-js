85d7e69c33a6cc7938ec84a040600ccfbe38b7cf
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2026-08-27 22:39:52 +0200

    refactor: remove aliasing of fminBrent and fminNelderMead for clarity

--- src/optimization.ts
+++ src/optimization.ts
@@ -1,4 +1,4 @@
-export { minimizeBrent as fminBrent } from './optimization/brent.js';
-export { minimizeNelderMead as fminNelderMead } from './optimization/nelderMead.js';
+export { minimizeBrent } from './optimization/brent.js';
+export { minimizeNelderMead } from './optimization/nelderMead.js';
 export type { BrentResult } from './optimization/brent.js';
 export type { NelderMeadResult } from './optimization/nelderMead.js';
