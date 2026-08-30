eaae6991d42c5430b4d3f45d0a6101d201bfb0bd
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2026-08-30 18:39:21 +0200

    feat: export LUDecomposition type from array2d module

--- src/array.ts
+++ src/array.ts
@@ -1,3 +1,4 @@
 export { Array1D } from './array/array1d.js';
 export { Array2D } from './array/array2d.js';
 export { Vec3 } from './array/vec3.js';
+export type { LUDecomposition } from './array/array2d.js';
