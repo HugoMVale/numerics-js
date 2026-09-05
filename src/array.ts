f0cdd56ae700f8e93c62e18fe9585fd85ee472cf
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2026-09-05 11:25:49 +0200

    feat: export QRDecomposition and Eigenvalue types from Matrix class

--- src/array.ts
+++ src/array.ts
@@ -1,4 +1,4 @@
 export { Vector } from './array/Vector.js';
 export { Matrix } from './array/Matrix.js';
 export { Vec3 } from './array/vec3.js';
-export type { LUDecomposition } from './array/Matrix.js';
+export type { LUDecomposition, QRDecomposition, Eigenvalue } from './array/Matrix.js';
