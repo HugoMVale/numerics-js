29d7cfda2ccfa438e086a5a88522631ce4128618
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2026-09-06 11:15:43 +0200

    rename vec3 to Vec3

--- src/linalg.ts
+++ src/linalg.ts
@@ -1,4 +1,4 @@
 export { Vector } from './linalg/Vector.js';
 export { Matrix } from './linalg/Matrix.js';
-export { Vec3 } from './linalg/vec3.js';
+export { Vec3 } from './linalg/Vec3.js';
 export type { LUDecomposition, QRDecomposition, Eigenvalue, Eigenpair } from './linalg/Matrix.js';
